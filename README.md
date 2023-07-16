ad814c3426e68ce71c10eab04f602aee3a66bf00
trangphandev <118053636+trangphandev@users.noreply.github.com>
2023-07-16 16:47:45 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# trangphandev.github.io
+# <h1 align="center">Personal Portfolio</h1>
+
+This is a repository for my personal portfolio website, showcasing my skills, projects, and certifications. The website is designed to provide an overview of my work and serve as a professional platform to showcase my abilities and experiences.
+
+## Features
+
+- **Home:** Discover the passion that drives me.
+- **About Me:** An introduction to who I am and what I do, highlighting my skills and expertise.
+- **Resume:** A downloadable version of my resume, providing detailed information about my education, work experience, and skills.
+- **Projects:** A collection of my past and ongoing projects, including descriptions, screenshots, and links to live demos or repositories.
+- **Contact:** A means for visitors to reach out to me, whether it be for collaboration, inquiries, or networking opportunities.
+
+
---
+++ src/components/About/index.js
@@ -0,0 +1,75 @@
+import "../../App.css";
+import DownloadIcon from "@mui/icons-material/Download";
+// import LearningCode from "../../assets/images/learn-coding-clipart.svg";
+import * as LottiePlayer from "@lottiefiles/lottie-player";
+
+export default function About() {
+  const getDate = () => {
+    var dob = new Date("05/18/2000");
+    var month_diff = Date.now() - dob.getTime();
+    var age_dt = new Date(month_diff);
+    var year = age_dt.getUTCFullYear();
+    var age = Math.abs(year - 1970);
+    return age;
+  };
+  return (
+    <div
+      id="about"
+      className="min-h-screen bg-white flex flex-col text-center gap-5 text-black text-lg  font-normal"
+    >
+      <div className="head text-5xl mt-12 font-bold" data-aos={"slide-down"}>
+        About Me
+      </div>
+      <div className="flex flex-row  gap-6 ml-8">
+        <div className="flex-col mt-14 flex flex-auto w-64 gap-6">
+          <h3 className="text-3xl font-medium" data-aos={"fade-left"}>
+            I'm <span className="text-yellow-600">Thao Vy Nguyen,</span> a Work lover
+          </h3>
+          <p
+            className="pt-5 leading-7 text-slate-900 text-justify"
+            data-aos={"fade-left"}
+          >
+            I love reading book, watching movie. I am
+            currently pursuing a bachelor's degree in Business Administration at VJIT. I am {getDate()}{" "}
+            years old. And I recently participated in a translation project at the company I work for.
+          </p>
+          <p
+            className="text-justify leading-7 text-slate-900"
+            data-aos={"fade-right"}
+          >
+            I am currently looking for a freelance translation, content writing, online page job.
+          </p>
+        </div>
+
+        <div
+          class="relative flex-auto w-32   sm:rounded-lg  pl-4"
+          data-aos={"slide-left"}
+        >
+          {/* <img
+            src={LearningCode}
+            alt="Learning Code"
+            className="motion-safe:animate-zoomy"
+          /> */}
+          <div className="motion-safe:animate-zoomy">
+            <lottie-player
+              autoplay
+              loop
+              mode="bounce"
+              src="https://assets10.lottiefiles.com/packages/lf20_w98qte06.json"
+              style={{ width: "350px" }}
+            />
+          </div>
+          <button className="py-3 mt-5 text-white mx-auto px-9 bg-blue-600 border-2 w-fit border-blue-500 rounded-3xl  hover:-translate-y-1.5 duration-[350ms] hover:duration-[350ms] hover:bg-blue-800 hover:scale-[1.023] focus:bg-blue-800 animate-zoomy">
+            <a
+              href={require("../../assets/files/Nguyen Thao Vy Resume.pdf")}
+              download={"Nguyen Thao Vy Resume.pdf"}
+            >
+              Download CV
+              <DownloadIcon />
+            </a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
